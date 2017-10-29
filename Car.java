c4560c39d8908cbbc5afaee7ee66d6dd2ec4fd44
bravoo84 <bhavikmehta98@gmail.com>
2017-10-30 00:51:00 +0530

    Commenting in car.java file
    commenting

--- Car.java
+++ Car.java
@@ -1,9 +1,12 @@
 package vehicle.park;
 
+
+//subclass for storing car information and extending vehicle class
 public class Car extends Vehicle{
 	private int numofDoors;
 	private String color;
 
+	//construtor for initialising variables of vehicle class under sublass car
 	public Car(String vehicleId, String vehicleBrand, String vehicleType, int year, int month, int date, int hours,
 			int numofDoors, String color) {
 		super(vehicleId, vehicleBrand, vehicleType, year, month, date, hours);
@@ -11,6 +14,7 @@ public class Car extends Vehicle{
 		this.color = color;
 	}
 
+	//function returning number of doors
 	public int getNumofDoors() {
 		return numofDoors;
 	}
@@ -19,10 +23,12 @@ public class Car extends Vehicle{
 		this.numofDoors = numofDoors;
 	}
 
+	//function returning color of car
 	public String getColor() {
 		return color;
 	}
 
+	//function for setting color of car
 	public void setColor(String color) {
 		this.color = color;
 	}
